23211fa69944de9598ea6968c26423d3c6d27d94
h577860 <42539100+h577860@users.noreply.github.com>
2020-05-07 21:32:31 +0200

    Added IoT device part to this project on Github
    Had not merged the two IntelliJ projects into the public repository

---
+++ dat110-project4-startcode-iotdevice/ACIoTDevice/src/no/hvl/dat110/aciotdevice/client/RestClient.java
@@ -0,0 +1,106 @@
+package no.hvl.dat110.aciotdevice.client;
+
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class RestClient {
+
+	public RestClient() {
+		// TODO Auto-generated constructor stub
+	}
+
+	private static String logpath = "/accessdevice/log";
+
+	public void doPostAccessEntry(String message) {
+
+		// TODO: implement a HTTP POST on the service to post the message
+		try (Socket s = new Socket(Configuration.host, Configuration.port)) {
+
+			// construct the HTTP request
+			Gson gson = new Gson();
+			AccessMessage accessmessage = new AccessMessage(message);
+			String jsonbody = gson.toJson(accessmessage, AccessMessage.class);
+
+			String httppostrequest =
+					"POST " + logpath + " HTTP/1.1\r\n" +
+							"Host: " + Configuration.host + "\r\n" +
+							"Content-type: application/json\r\n" +
+							"Content-length: " + jsonbody.length() + "\r\n" +
+							"Connection: close\r\n" +
+							"\r\n" +
+							jsonbody +
+							"\r\n";
+
+			// send the response over the TCP connection
+			OutputStream output = s.getOutputStream();
+
+			PrintWriter pw = new PrintWriter(output, false);
+			pw.print(httppostrequest);
+			pw.flush();
+
+
+		} catch (IOException ex) {
+			System.err.println(ex);
+		}
+	}
+	
+	private static String codepath = "/accessdevice/code";
+	
+	public AccessCode doGetAccessCode() {
+
+		AccessCode code = null;
+		
+		// TODO: implement a HTTP GET on the service to get current access code
+
+		try (Socket s = new Socket(Configuration.host, Configuration.port)) {
+
+			// construct the GET request
+			String httpgetrequest = "GET " + codepath + " HTTP/1.1\r\n" +
+					"Accept: application/json\r\n" +
+					"Host: localhost\r\n" +
+					"Connection: close\r\n" +
+					"\r\n";
+
+			// send the HTTP request
+			OutputStream output = s.getOutputStream();
+			PrintWriter pwr = new PrintWriter(output, false);
+			pwr.print(httpgetrequest);
+			pwr.flush();
+
+			// read the HTTP response
+			InputStream input = s.getInputStream();
+			Scanner sc = new Scanner(input);
+			StringBuilder jsonresponse = new StringBuilder();
+			boolean header = true;
+
+			while (sc.hasNext()) {
+				String nextline = sc.nextLine();
+				if (header) {
+					System.out.println(nextline);
+				} else {
+					jsonresponse.append(nextline);
+				}
+				// simplified approach to identifying start of body: the empty line
+				if (nextline.isEmpty()) {
+					header = false;
+				}
+			}
+
+			sc.close();
+
+			Gson gson = new Gson();
+			code = gson.fromJson(jsonresponse.toString(), AccessCode.class);
+
+		} catch (IOException ex) {
+			System.err.println(ex);
+			}
+
+			return code;
+	}
+}
